56fdb348398e324cbbb9a988877234261d88b625
Ma <mateuszmitek@MacBook-Pro-mateusz.local>
2018-07-11 19:24:26 +0200

    update

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ News.set("CountLikes", 0);
 News.save().then(function(results) {
 	console.log("GIT: "+ results+" x:"+x+" max:"+ArrayNews.length+" y:"+y);
 
-	if(ArrayNews.length == x){
+	if(ArrayNews.length == (x-1)){
 		console.log("Get next NEWS");
 		GetNewsApi(y+1);
 	}else{
