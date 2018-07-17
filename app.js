82870741eab1905e21b48ff82c7976dccd23c48e
Ma <mateuszmitek@MacBook-Pro-mateusz.local>
2018-07-17 11:23:27 +0200

    update

--- app.js
+++ app.js
@@ -290,8 +290,6 @@ function saveCalendar(id, Title, AllCOINS,  eDate, cDate, Content, Source,  isHo
 	var COINS = [];
     var e = new Date(eDate);
     var c = new Date(cDate);
-    e.setDate(e.getDate() + 1);
-    c.setDate(c.getDate() + 1);
 
     for(var c in AllCOINS){
     	COINS.push(AllCOINS[c].symbol);
