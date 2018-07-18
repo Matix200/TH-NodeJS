e19ba01f5ff3506588aa8e0e91432536b3901044
Ma <mateuszmitek@MacBook-Pro-mateusz.local>
2018-07-18 16:47:11 +0200

    update

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ request('https://cryptopanic.com/api/posts/?auth_token=2f75a7bc9bc217ceebad0c221
      // console.log(info);
      
       for(var i in info){
+      	var metadata = info[i].metadata;
   		ArrayNews.push({
   			"ID" : info[i].id,
   			"created_at" : info[i].created_at,
@@ -60,8 +61,8 @@ request('https://cryptopanic.com/api/posts/?auth_token=2f75a7bc9bc217ceebad0c221
   			"source" : info[i].source,
   			"currencies" : info[i].currencies,
   			"published_at" : info[i].published_at,
-  			"images" : info[i].metadata[image],
-  			"description" : info[i].metadata[description],
+  			"images" : metadata.image,
+  			"description" : metadata.description,
   			"url" : info[i].url
 
   		})
