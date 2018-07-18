eb66695903eb8913564231537749eba68b7b2098
Ma <mateuszmitek@MacBook-Pro-mateusz.local>
2018-07-18 16:49:55 +0200

    update

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ request('https://cryptopanic.com/api/posts/?auth_token=2f75a7bc9bc217ceebad0c221
       var info = JSON.parse(body)
       info = info.results;
      // console.log(info);
+     console.log(info);
      
       for(var i in info){
       	var metadata = info[i].metadata;
